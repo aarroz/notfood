f351502a10f7fafd6362984c362afc8ec3dea88f
Synlinux <thehman1000@gmail.com>
2018-04-07 19:35:46 -0700

    Added stick & base sprite

---
+++ openart tilema/notfood.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="notfood" tilewidth="32" tileheight="32" spacing="32" margin="32" tilecount="4" columns="4">
+ <image source="spritesheet.png" width="278" height="74"/>
+</tileset>
---
+++ openart tilema/spritesheet.tsx
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spritesheet" tilewidth="32" tileheight="32" spacing="1" tilecount="16" columns="8">
+ <image source="spritesheet.png" width="263" height="65"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="13"/>
+  <terrain name="New Terrain" tile="12"/>
+  <terrain name="New Terrain" tile="4"/>
+  <terrain name="New Terrain" tile="5"/>
+  <terrain name="New Terrain" tile="14"/>
+  <terrain name="New Terrain" tile="15"/>
+  <terrain name="New Terrain" tile="7"/>
+  <terrain name="New Terrain" tile="6"/>
+  <terrain name="New Terrain" tile="6"/>
+  <terrain name="New Terrain" tile="3"/>
+  <terrain name="New Terrain" tile="2"/>
+  <terrain name="New Terrain" tile="1"/>
+  <terrain name="New Terrain" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain="12,12,12,12"/>
+ <tile id="1" terrain="11,11,11,11"/>
+ <tile id="2" terrain="10,10,10,10"/>
+ <tile id="3" terrain="9,9,9,9"/>
+ <tile id="4" terrain="2,2,2,2"/>
+ <tile id="5" terrain="3,3,3,3"/>
+ <tile id="6" terrain="7,7,7,7"/>
+ <tile id="7" terrain="6,6,6,6"/>
+ <tile id="11" terrain="8,8,8,8"/>
+ <tile id="12" terrain="1,1,1,1"/>
+ <tile id="13" terrain="0,0,0,0"/>
+ <tile id="14" terrain="4,4,4,4"/>
+ <tile id="15" terrain="5,5,5,5"/>
+</tileset>
